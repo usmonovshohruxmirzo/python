03413b646411c791ceb171123443749e51e6c4e2
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-10-13 19:29:51 +0500

    feat: Numeric types

--- lessons/05 - Numbers.py
+++ lessons/05 - Numbers.py
@@ -11,17 +11,51 @@ x = 1 # Int, or integer, is a whole number, positive or negative, without decima
 y = 2.5 # Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
 z = 3j # Complex numbers are written with a "j" as the imaginary part:
 
-print(x, type(x))
-print(y, type(y))
-print(z, type(z))
+# print(x, type(x))
+# print(y, type(y))
+# print(z, type(z))
 
 # Type Conversion
 
-print(float(x))
-print(int(y))
-print(complex(x))
+# print(float(x))
+# print(int(y))
+# print(complex(x))
 
 # Random Number
-import random
+# import random
+# print(random.randrange(1, 10))
 
-print(random.randrange(1, 10))
+
+# integers
+
+bin = 0b1010
+oct = 0o36701
+hex = 0xf6a10d
+
+# print(bin, type(bin))
+# print(oct, type(oct))
+# print(hex, type(hex))
+
+# n = 10 ** 10000
+# print(n)
+
+# import sys
+# # print(sys.get_int_max_str_digits())
+# sys.set_int_max_str_digits(20000)
+# n = 10 ** 10000
+# print(n)
+#
+# result1 = 1000000000000
+# result2 = 1_000_000_000_000
+# print(result1)
+# print(result2)
+
+# input = input("enter number: ")
+# print(int(input).as_integer_ratio())
+# print(int(input).bit_count())
+# print(int(input).bit_length())
+# print(int(input).to_bytes())
+#
+# output1 = int(input).to_bytes()
+# output2 = int.from_bytes(output1)
+# print(output2, type(output2)
