678ba7e2eab222a424485559b64bd04f5546500a
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-11-15 21:08:53 +0500

    feat(lessons): Use cases of Container data types

---
+++ lessons/Use Cases for Container Data Types.py
@@ -0,0 +1,23 @@
+# Lists
+#   Lists are commonly used to store collections of homogeneous items, such as numbers or strings.
+#   They are mutable, meaning the elements of a list can be modified after it is created.
+#   Lists are useful for situations where you need to add, remove, or modify elements frequently.
+#   Use lists when you need an ordered collection that can be changed.
+
+# Tuples
+#   Tuples are used to store collections of heterogeneous items, such as different data types or values with different meanings.
+#   They are immutable, meaning the elements of a tuple cannot be changed after it is created.
+#   Tuples are often used to represent fixed collections of items, like coordinates or configurations.
+#   Use tuples to ensure data integrity or when you need an ordered collection that should not be modified.
+
+# Sets
+#   Sets are used to store collections of unique items in which each item appears only once.
+#   They are mutable, which means items can be added or removed from a set.
+#   Sets are useful for tasks that involve eliminating duplicates or testing membership.
+#   Use sets when you need to perform operations like union, intersection, or difference.
+
+# Dictionaries
+#   Dictionaries are used to store key-value pairs in which each value is associated with a unique key.
+#   They are mutable, which means key-value pairs can be added, modified, or removed.
+#   Dictionaries are often used to represent mappings between related pieces of information, such as user profiles or configuration settings.
+#   Use dictionaries when you need to access values based on their corresponding keys quickly or when you need to store keys and values where a clear association exists between each key and its corresponding value.
