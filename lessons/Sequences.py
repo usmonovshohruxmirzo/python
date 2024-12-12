e41870eb5c85e92d1867e771d8c4d54f68b7d98a
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-12-12 21:49:09 +0500

    feat: Sequences

---
+++ lessons/Sequences.py
@@ -0,0 +1,52 @@
+# my_str = "Happy New Year"
+# res = my_str.find("e")
+# print(res)
+
+# my_str = "Happy New Year"
+# if "p" in my_str:
+#     print("!")
+# else:
+#     print("?")  
+
+a = [1,23, 5, "i am new el", True]
+
+print(a[::2]) # by 2 step
+
+
+# my_list = [3, 5, 7, 9]
+# res = my_list[1] + len(my_list[1:3])
+# my_list.append(11)
+# res += len(my_list)
+# print(res)
+
+# my_list = [5, 3, 9, 7, 1]
+# my_list.sort()
+# res = my_list[1] + my_list[3]
+# print(res)
+
+# my_list = [5, 3, 9, 7, 1]
+# my_list.extend([6, 8, 2, 4])
+
+# my_list.sort()
+# res = my_list.pop() + my_list.pop()
+
+# print(res)
+
+# my_list = [5, 3, 9, 7, 1]
+# my_list.extend([6, 8, 2, 4])
+
+# my_list.sort()
+# res = [i for i in my_list if i > 5]
+
+
+# print(len(res))
+
+# list1 = [1, 3, 5]
+# list2 = [1, 3, 5]
+
+# print(list1 == list2 and list1 is list2)
+
+# tup1 = (1, 3, 5)
+# tup2 = (1, 3, 5)
+
+# print(tup1 == tup2 and tup1 is tup2)
